9bd3a220b1fbc9a4cfb966155891009361f14170
Bendik Nygård <bendikn@gmail.com>
2026-04-28 14:25:59 +0200

    go

--- packet_error_rate_receiver.py
+++ packet_error_rate_receiver.py
@@ -33,7 +33,7 @@ from interleaver import Interleaver
 from scrambler import LFSRScrambler
 from project_logger import configure_project_logging, get_configured_log_level
 
-NUMBER_OF_DATAGRAMS = 500
+NUMBER_OF_DATAGRAMS = 100
 
 SPINNER = ['|', '/', '-', '\\']
 
